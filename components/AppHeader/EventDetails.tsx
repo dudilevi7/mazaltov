aec8515d1a12393e75668ae56c432f6ccfd12d23
dudilevi7 <dlevis10@gmail.com>
2026-02-13 17:12:14 +0200

    another ones

--- components/AppHeader/EventDetails.tsx
+++ components/AppHeader/EventDetails.tsx
@@ -9,19 +9,20 @@ import { LanguageDirection } from '@/types/General'
 const EventDetails = () => {
   const { eventSettings, languageDirection, isSidebarOpen } = useAppContext()
   const eventTypeDisplayName = getEventTypeDisplayName(eventSettings.eventType, languageDirection)
-  const placementOfDetails = languageDirection === LanguageDirection.HEB ? 'right-5' : 'left-5'
+  const placementOfDetails = languageDirection === LanguageDirection.HEB ? 'right-0' : 'left-0'
   if (!isSidebarOpen) return null
   return (
     <div
-      className={`absolute bottom-8 ${placementOfDetails} flex flex-col bg-gray-100 border border-gray-200 px-1.5 py-0.5 rounded-md z-50 animate-fade-in-0.5`}
+      className={`absolute bottom-8 ${placementOfDetails} flex flex-col bg-linear-to-r from-blue-200 to-blue-300
+         text-gray-700 px-3 py-1.5 rounded-e-md z-50 animate-fade-in-0.5 shadow-lg shadow-blue-500/20`}
       dir={languageDirection}>
-      <div className="flex flex-row gap-0.5">
-        <span className="text-sm text-gray-500 rounded-md">{eventTypeDisplayName}</span>
-        <span className="text-sm text-gray-500 rounded-md">-</span>
-        <span className="text-sm text-gray-500 rounded-md">{getEventDisplayName(eventSettings)}</span>
+      <div className="flex flex-row gap-0.5 font-bold">
+        <span className="text-sm rounded-md">{eventTypeDisplayName}</span>
+        <span className="text-sm rounded-md">-</span>
+        <span className="text-sm rounded-md">{getEventDisplayName(eventSettings)}</span>
       </div>
-      <span className="text-xs text-gray-500 rounded-md">{eventSettings.eventHall}</span>
-      <span className="text-xs text-gray-500 rounded-md">{moment(eventSettings.eventDate).format('DD/MM/YYYY')}</span>
+      <span className="text-xs rounded-md">{eventSettings.eventHall}</span>
+      <span className="text-xs rounded-md">{moment(eventSettings.eventDate).format('DD/MM/YYYY')}</span>
     </div>
   )
 }
